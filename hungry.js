3bf70d569d4a537791f67424e7074fbeae77e587
KevinASC6 <kpepin21@riverdale.edu>
2019-07-10 14:10:44 -0400

    Conditionals

---
+++ hungry.js
@@ -0,0 +1,9 @@
+const wallet = 1.44;
+const hungry = true;
+const needBev = false;
+if(wallet==4.44 && ( hungry == true || needBev == true)){
+    console.log('Welcome to Wendys!');
+}
+else {
+    console.log("Sir, this is a Wendy's");
+}
